461c6e7539397d2b2aebe2902c63614cacedf2cd
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-09-03 14:43:15 +0000

    7. islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def islower(c):
-    for i in c:
-        if i.islower():
-            return True
-        elif i == "":
+    for i in c:        
+        if i == "":
             return
+        elif i.islower():
+            return True
         else:
             return False

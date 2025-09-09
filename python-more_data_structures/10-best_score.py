330a9d85e4c23801dffd0ae448de2b1074cfb06b
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-09-09 13:07:57 +0000

    10. Best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,4 +2,8 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+    value = list(a_dictionary.keys())[0]
+    for key in a_dictionary:
+        if key > value:
+            value = key
+    return value

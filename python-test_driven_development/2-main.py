d74cc36844425a153866dcc7b53cb7b7218c1966
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-09-12 17:57:01 +0000

    1. Divide a matrix

--- python-test_driven_development/2-main.py
+++ python-test_driven_development/2-main.py
@@ -3,7 +3,8 @@ matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [
     [1, 2, 3],
-    [4, 5, 6]
+    [4, 5],
+    [6, 7, 8]
 ]
-print(matrix_divided(matrix, 3))
+print(matrix_divided(matrix, 2))
 print(matrix)

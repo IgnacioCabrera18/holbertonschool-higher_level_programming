0fadba0352f1e8a6212ffee4e9aa8e43b39fd463
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-09-12 17:52:36 +0000

    1. Divide a matrix

---
+++ python-test_driven_development/2-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+matrix_divided = __import__('2-matrix_divided').matrix_divided
+
+matrix = [
+    [2.2, 3.3],
+    [5.5, 6.6],
+]
+print(matrix_divided(matrix, [2]))
+print(matrix)

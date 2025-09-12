ff7a8b280fbe7c2f8093f1f52e60a1c71923f918
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-09-12 16:27:56 +0000

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+
+"""module of add two integers"""
+
 def add_integer(a, b=98):
+    """add two integers function"""
+    
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):

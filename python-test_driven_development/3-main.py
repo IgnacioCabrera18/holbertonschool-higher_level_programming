5597a25cb0d8048ccdcf27cca4644189928a2bf4
IgnacioCabrera18 <ignaciocabrera832006@gmail.com>
2025-09-13 17:49:56 +0000

    2. Say my name

--- python-test_driven_development/3-main.py
+++ python-test_driven_development/3-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 say_my_name = __import__('3-say_my_name').say_my_name
 
-say_my_name("Ignacio", "Federico", "Cabrera")
+say_my_name("Bob")
 say_my_name("Walter", "White")
 say_my_name("Bob")
 try:
--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+
+"""module of print my name is: ...."""
+
+
 def say_my_name(first_name, last_name=""):
+    """Prints "My name is <first name> <last name>"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):

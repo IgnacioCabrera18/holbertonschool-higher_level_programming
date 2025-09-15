3b9c2b108cdabcb9b7de5aafab7978f8732f70a3
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-09-15 14:05:05 +0000

    1. Square with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """Represente a square"""
+    def __init__(self, size):
+        """Initalize a new square"""
+        self.__size = size

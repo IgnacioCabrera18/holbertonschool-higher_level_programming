a838a7c7de040010af07001ebc4485f02acd4502
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-09-23 17:03:00 +0000

    6. Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Define a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Reprecente the class"""
+    def area(self):
+        """Raicce Exception"""
+        raise Exception("area() is not implemented")

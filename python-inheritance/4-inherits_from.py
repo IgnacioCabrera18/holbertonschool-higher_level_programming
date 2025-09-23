cf7b53065d7d7bed69fa98b57d96710014d062ea
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-09-23 15:00:51 +0000

    4. Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 
 def inherits_from(obj, a_class):
     """Return True or Flase"""
-    if not type(obj) is a_class:
+    if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     else:
         False

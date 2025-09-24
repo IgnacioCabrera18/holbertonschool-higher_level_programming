19f2371164cd8a9dbff1bf7ccbf2584d5bbc2a09
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-09-24 11:22:15 +0000

    11. Square #2

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Define a class Square that inherits from Rectangle"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+        super().__str__

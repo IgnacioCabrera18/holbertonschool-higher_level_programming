dfcce9f149d1fb22a03dc6fb400a7e4117dc09e5
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-09-29 00:02:58 +0000

    3. CountedIterator - Keeping Track of Iteration

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""""""
+
+
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.count = 0
+
+    def __next__(self):
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise StopIteration
+
+    def __iter__(self):
+        return self
+
+    def get_count(self):
+        return self.count

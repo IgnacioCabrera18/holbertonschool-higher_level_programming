1ff789854b6abf38ca2214916164ec24e3bfd5ce
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-09-29 00:29:38 +0000

    1. Shapes, Interfaces, and Duck Typing

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -5,7 +5,6 @@ from abc import ABC, abstractmethod
 
 
 class Shape(ABC):
-    """"""
     @abstractmethod
     def area(self):
         pass
@@ -14,26 +13,20 @@ class Shape(ABC):
     def perimeter(self):
         pass
 
+
 class Circle(Shape):
-    """"""
     def __init__(self, radius):
-        if not isinstance(radius, (int, float)) or radius <= 0:
-            raise ValueError("Radius must be a positive number.")
         self.radius = radius
 
     def area(self):
-        return math.pi * (self.radius ** 2)
+        return math.pi * self.radius ** 2
 
     def perimeter(self):
         return 2 * math.pi * self.radius
 
+
 class Rectangle(Shape):
-    """"""
     def __init__(self, width, height):
-        if not isinstance(width, (int, float)) or width <= 0:
-            raise ValueError("Width must be a positive number.")
-        if not isinstance(height, (int, float)) or height <= 0:
-            raise ValueError("Height must be a positive number.")
         self.width = width
         self.height = height
 
@@ -43,6 +36,7 @@ class Rectangle(Shape):
     def perimeter(self):
         return 2 * (self.width + self.height)
 
-def shape_info(shape_object):
-    print(f"Area: {shape_object.area()}")
-    print(f"Perimeter: {shape_object.perimeter()}")
+
+def shape_info(shape):
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")

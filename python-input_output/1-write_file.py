652534ff44a3441fa6b08d6d7f7a9f1c4f016e25
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-09-30 22:18:50 +0000

    1. Write to a file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-""""""
+"""Module write_file"""
 
 
 def write_file(filename="", text=""):
-    """"""
+    """Function write a file"""
     with open(filename, 'w', encoding="utf-8") as f:
         num_write = f.write(text)
     return num_write

00e84749629867546e57e0728e82c09ce03bfe72
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-09-30 22:05:18 +0000

    0. Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+
+"""Module read_file."""
+
+
 def read_file(filename=""):
+    """Function read a file"""
     with open(filename, "r", encoding='UTF8') as f:
         print(f.read())

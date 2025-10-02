0ef6dccc93070c88bfb484ee471c40c46919d7d2
IgnacioCarera18 <ignaciocabrera1832006@gmail.com>
2025-10-02 13:36:31 +0000

    12. Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+
+    for i in range(n):
+        act_row = [1] * (1 + i)
+        if i > 1:
+            prev_row = triangle[i - 1]
+            for j in range(1, i):
+                act_row[j] = prev_row[j - 1] + prev_row[j]
+        triangle.append(act_row)
+    return triangle

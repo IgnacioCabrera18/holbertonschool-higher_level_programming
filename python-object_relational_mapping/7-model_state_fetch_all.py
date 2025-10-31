bc1fc44de60a0870bbbd7c90182d2d46cff823c1
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-10-31 14:49:40 +0000

    7. All states via SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, db_name),
+        'mysql+mysqldb://{}:'
+        '{}@localhost/{}'.format(username, password, db_name),
         pool_pre_ping=True
     )
     Base.metadata.create_all(engine)

dc46b69f8885830b064870722e5ce9f2c4c0c211
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-10-31 15:26:55 +0000

    10. Get a state

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Lists all State objects from the database using SQLAlchemy ORM"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:'
+        '{}@localhost/{}'.format(username, password, db_name),
+        pool_pre_ping=True
+    )
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    state = (
+        session.query(State).filter
+        (State.name == state_name).first()
+    )
+
+    if state:
+        print(state.id)
+    else:
+        print("Not found")
+
+    session.close()

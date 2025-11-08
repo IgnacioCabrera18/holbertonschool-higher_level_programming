043c567a1a8fcc6a4355ae0c361b83c1255a6177
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-11-08 17:15:12 +0000

    Done

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-if (process.argv.length < 3) {
-    console.log('No argument');
-}
-else if (process.argv.length === 3) {
-    console.log('Argument found');
-}
-else {
-    console.log('Arguments found');
+const numberOfArguments = process.argv.length - 2;
+
+if (numberOfArguments === 0) {
+  console.log("No argument");
+} else if (numberOfArguments === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
 }
